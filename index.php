a5fc2c0f9144948174560dda86d327c2f08f7aa3
alihajj95 <66716506+alihajj95@users.noreply.github.com>
2020-06-11 19:57:32 -0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL ^ E_NOTICE);
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>myDropBox - log in</title>
+	<title>CIS497G DropBox</title>
 	
 	<link href="css/admin_theme_white.css" rel="stylesheet" type="text/css">
 
